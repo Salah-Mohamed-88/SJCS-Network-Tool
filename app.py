58d6e589f1259d7533b1e6b83c8675b1fc597920
Salah-Mohamed-88 <dodyragax@gmail.com>
2026-04-17 21:13:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,32 @@ st.markdown("### 📡 Network Status")
 
 col1, col2 = st.columns(2)
 
-col1.metric("Latency", "120 ms")
-col2.metric("Jitter", "45 ms")
+latency = 120
+jitter = 45
+
+col1, col2 = st.columns(2)
+
+col1.metric("Latency", f"{latency} ms")
+col2.metric("Jitter", f"{jitter} ms")
+
+
+if st.button("Run Auto Fix 🤖"):
+    st.warning("⚠️ Fixing...")
+
+    # simulate improvement
+    latency = 80
+    jitter = 25
+
+    col1.metric("Latency", f"{latency} ms")
+    col2.metric("Jitter", f"{jitter} ms")
+
+    latency = 40
+    jitter = 10
+
+    col1.metric("Latency", f"{latency} ms")
+    col2.metric("Jitter", f"{jitter} ms")
+
+    st.success("✅ Issue Resolved (Latency: 40ms | Jitter: 10ms)")
 
 st.warning("⚠️ Network Issue Detected")
 
